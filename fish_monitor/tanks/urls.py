f0167bb0d86e05dc07ed0789c63e59e80b494479
sam simmons <samuel.j.simmons@gmail.com>
2017-09-10 15:06:48 +1200

    add route for water change

--- fish_monitor/tanks/urls.py
+++ fish_monitor/tanks/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^tanks/$', views.TankList.as_view()),
     url(r'^tanks/(?P<tank_id>[0-9]+)/$', views.TankDetail.as_view()),
+    url(r'^tanks/(?P<tank_id>[0-9]+)/change/$', views.TankWaterChange.as_view()),
     url(r'^tanks/(?P<tank_id>[0-9]+)/fish/$', views.FishList.as_view()),
     url(r'^tanks/(?P<tank_id>[0-9]+)/fish/(?P<fish_id>[0-9]+)/$', views.FishDetail.as_view()),
 ]
--- fish_monitor/tanks/views.py
+++ fish_monitor/tanks/views.py
@@ -22,6 +22,21 @@ class TankDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAdminOrReadOnly, )
     lookup_url_kwarg = 'tank_id'
 
+class TankWaterChange(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    def get_object(self, pk):
+        try:
+            return Tank.objects.get(pk=pk)
+        except Tank.DoesNotExist:
+            raise Http404
+    def post(self, request, tank_id, format=None):
+        tank = self.get_object(tank_id)
+        serializer = TankSerializer(tank, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class FishList(generics.ListCreateAPIView):
     queryset = Fish.objects.all()
     serializer_class = FishSerializer
