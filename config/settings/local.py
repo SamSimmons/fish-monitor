3b3b8ec8d61d9c93066385d5d2ed4be6d3051f9e
sam simmons <samuel.j.simmons@gmail.com>
2017-11-02 11:17:38 +1300

    add default config, add defaults for different envs

---
+++ config/settings/local.py
@@ -0,0 +1,6 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'cg9j4-)glyp@1olz)iiz6z9u^zwu!hf#9&knv+k-_4jd-0juf_'
+
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
